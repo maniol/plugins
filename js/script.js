467746f750bae75bbcfc178319aeb30eddb9b5e8
magda <magdalena.aniol@gmail.com>
2018-06-12 12:21:48 +0200

    add progress bar

--- js/script.js
+++ js/script.js
@@ -10,3 +10,10 @@ var restartButton = document.querySelector('.restart');
 restartButton.addEventListener( 'click', function() {
 	flkty.selectCell('#tempelhof');
 });
+
+var progressBar = document.querySelector('.progress-bar')
+
+flkty.on( 'scroll', function( progress ) {
+  progress = Math.max( 0, Math.min( 1, progress ) );
+  progressBar.style.width = progress * 100 + '%';
+});
