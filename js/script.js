b147e89056d1b0dce6d0dc7bfdc6b26b1009c91b
magda <magdalena.aniol@gmail.com>
2018-06-16 17:22:30 +0200

    add map with markers

--- js/script.js
+++ js/script.js
@@ -26,3 +26,21 @@ flkty.on( 'scroll', function( progress ) {
   progress = Math.max( 0, Math.min( 1, progress ) );
   progressBar.style.width = progress * 100 + '%';
 });
+
+(function() {
+  var infos = document.getElementById('infos');
+ // Initialize and add the map
+  window.initMap = function () {
+    // The location of Tempelhof
+    var tempelhofCoord = {lat: 52.462553, lng: 13.392475};
+    var uluru = {lat: -25.344, lng: 131.036};
+    // The map, centered at Tempelhof
+    var map = new google.maps.Map(
+    document.getElementById('map'), {zoom: 14, center: tempelhofCoord});
+    // Assign markers to locations
+    for (var location in carouselData) {
+      var markerName = carouselData[location]['id'] + 'Marker';
+      markerName = new google.maps.Marker({position: carouselData[location]['coords'], map: map});
+    }
+  };
+})();
